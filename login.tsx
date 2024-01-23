13a2db47c8197476a4b879f1c0c94c0ea9a93853
tuanaakyildiz <139168702+tuanaakyildiz@users.noreply.github.com>
2024-01-23 12:55:31 +0300

    Add files via upload

--- login.tsx
+++ login.tsx
@@ -2,40 +2,27 @@ import {Text, View, TextInput} from 'react-native';
 import React, {useState} from 'react';
 //import axios from 'axios';
 import {Button} from 'react-native';
-// import {useNavigation} from '@react-navigation/native';
-// import Main from "./main.tsx";
-const LoginScreen = () => {
+export default function LoginScreen({navigation}: any) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  // const navigation = useNavigation();
-  const handleSubmit = () => {
-    /*async () => {
-        const response = await axios.post('', {
-            username,
-            password,
-          });*/
-    };
-
-    return (
-      <View>
-        <TextInput
-          placeholder="Username"
-          value={username}
-          onChangeText={setUsername}
-        />
-        <TextInput
-          placeholder="Password"
-          secureTextEntry={true}
-          value={password}
-          onChangeText={setPassword}
-        />
-        <Button title={'Submit'} onPress={handleSubmit} />
-        {/*<Button title="Go to About" onPress={} />*/}
-        <Text>username:{username}</Text>
-        <Text>password:{password}</Text>
-      </View>
-    );
-  };
-};
-export default LoginScreen;
+  return (
+    <View>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title={'Submit'} onPress={() => navigation.navigate('Main')} />
+      {/*<Button title="Go to About" onPress={} />*/}
+      <Text>username:{username}</Text>
+      <Text>password:{password}</Text>
+    </View>
+  );
+}
